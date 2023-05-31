931842e4f1e6a8d433bde7506029b3d1fabf9c79
Respinoza2704 <respinoza@creasoftweb.com>
2023-05-30 23:15:41 -0500

    resume js files

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,58 +1,12 @@
-let countaddrs = 2;
-let countadelivery = 2;
+
 $(document).ready(function () {
     URL_OBEN = new URLSearchParams(window.location.search);
-    if (URL_OBEN.get('action') === "directlogin") {
-        $('.m-crea-section.right').addClass('inactive')
-        $('.m-crea-section.right').children('.m-crea__content').removeClass('active')
-        $('.m-crea-section.right').children('.m-crea__content:last-child').addClass('active')
-        $('.m-crea-section.left').addClass('inactive')
-    } else
-        if (URL_OBEN.get('action') === "openmodal") {
-            $('.m-crea-modal').fadeIn();
-        }
+
 })
 
 function OpenSection(e) {
     switch ($(e).attr('data-open')) {
-        case 'login':
-            new WOW().init();
-            if ($('.m-crea-section.left').hasClass('inactive')) {
-                $('.m-crea-section.left').removeClass('inactive')
-            } else {
-                $('.m-crea-section.left').addClass('inactive')
-            }
-            if ($('.m-crea-section.right').hasClass('inactive')) {
-                $('.m-crea-section.right').removeClass('inactive')
-            } else {
-                $('.m-crea-section.right').addClass('inactive')
-            }
-            if ($('.m-crea-section.right').children('.m-crea__content:last-child').hasClass('active')) {
-                $('.m-crea-section.right').children('.m-crea__content:first-child').addClass('active')
-                $('.m-crea-section.right').children('.m-crea__content:last-child').removeClass('active')
-            } else {
-                $('.m-crea-section.right').children('.m-crea__content:first-child').removeClass('active')
-                $('.m-crea-section.right').children('.m-crea__content:last-child').addClass('active')
-            }
-            break;
-        case 'register':
-            $('.m-crea-section.left').addClass('inactive')
-            window.location.href = "/register.html";
-            break;
-        //         case 'bussiness':
-        //             $('.m-crea-section.left').addClass('inactive')
-        //             window.location.href = "/register-bussiness.html" ;
-        //             break; 
-        //             case 'rescue':
-        //                 $('.m-crea-section.left').addClass('inactive')
-        //                 window.location.href = "/rescue-account.html";
-        //             break; 
-        //             case 'directlogin':
-        //                 $('.m-crea-section.left').addClass('inactive')
-        //                 window.location.href = "/?action=directlogin" ;
-        //     break;
-
-
+        
         case 'profile':
             window.location.href = "/admin/edit-profile.html";
             break;
@@ -60,13 +14,6 @@ function OpenSection(e) {
         case 'logout':
             window.location.href = "/admin/index.html";
             break;
-        // case 'new-historical':
-        //     window.location.href = "/nueva-solicitud.html" ;
-        //     break;
-        // case 'historical':
-        //     window.location.href = "/historico-solicitudes.html" ;
-        //     break;
-
     }
 }
 
@@ -161,103 +108,6 @@ $('form').submit(function (e) {
                     }
             break;
 
-        /** recuperar contraseña validator */
-        case 'rescue':
-            var EMAIL = $('#email');
-            var MESSAGE = $('.text-warning')
-            if (!ValidateEmail(EMAIL.val())) {
-                MESSAGE.text('Correo inválido. Por favor ingrese un correo con el formato correcto.')
-                e.preventDefault()
-                EMAIL.addClass('border-warning')
-                return
-            } else {
-                $('.m-crea-modal').fadeIn();
-                e.preventDefault()
-                timeooutredirect('login')
-            }
-            break;
-        /** post recuperador de contraseña validator */
-        case 'post-rescue':
-            var CONTRASEÑA1 = $('#password');
-            var CONTRASEÑA2 = $('#password2');
-            var MESSAGE1 = $('#pasword1-message')
-            var MESSAGE2 = $('#pasword2-message')
-            if (itsempyornull(CONTRASEÑA1.val())) {
-                MESSAGE1.text('El campo no puede quedar vacío.')
-                e.preventDefault()
-                CONTRASEÑA1.addClass('border-warning')
-                return
-            } else if (CONTRASEÑA1.val().length < 8) {
-                MESSAGE1.text('Debe contenener un mínimo de 8 caracteres.')
-                e.preventDefault()
-                CONTRASEÑA1.addClass('border-warning')
-                return
-            } else
-                if (!(CONTRASEÑA1.val() === CONTRASEÑA2.val())) {
-                    MESSAGE2.text('Las contraseñas no coinciden.')
-                    CONTRASEÑA1.addClass('border-warning')
-                    CONTRASEÑA2.addClass('border-warning')
-                    e.preventDefault()
-                    return
-                } else {
-                    $('.m-crea-modal').fadeIn();
-                    e.preventDefault()
-                    timeooutredirect('login')
-                }
-            break;
-        /** Registrar validator */
-        case 'register':
-            var NAME = $('#name');
-            var LASTNAME = $('#lastname')
-            var EMAIL = $('#email')
-            var IDFISCAL = $('#fiscal')
-            var CARGO = $('#position');
-            var PHONE = $('#phone')
-            if (itsempyornull(NAME.val())) {
-                NAME.siblings('.text-warning').text('El campo nombre no puede quedar vacio')
-                NAME.addClass('border-warning')
-                e.preventDefault()
-                return
-            } else
-                if (itsempyornull(LASTNAME.val())) {
-                    LASTNAME.siblings('.text-warning').text('El campo apellido no puede quedar vacio')
-                    LASTNAME.addClass('border-warning')
-                    e.preventDefault()
-                    return
-                } else
-                    if (!ValidateEmail(EMAIL.val())) {
-                        EMAIL.siblings('.text-warning').text('Debe introducir una direccion de email válida')
-                        e.preventDefault()
-                        EMAIL.addClass('border-warning')
-                        return
-                    } /*else 
-            if (itsempyornull(IDFISCAL.val())) {
-                IDFISCAL.siblings('.text-warning').text('El campo id fiscal no puede quedar vacio') 
-                IDFISCAL.addClass('border-warning')
-                e.preventDefault()
-                return
-            } else
-            if (itsempyornull(CARGO.val())) {
-                CARGO.siblings('.text-warning').text('El campo cargo no puede quedar vacio') 
-                CARGO.addClass('border-warning')
-                e.preventDefault()
-                return
-            } else*/
-            /* if (itsempyornull(PHONE.val())) {
-                 PHONE.siblings('.text-warning').text('El campo telefono no puede quedar vacio') 
-                 PHONE.addClass('border-warning')
-                 e.preventDefault()
-                 return
-             }*/
-            break;
-        case 'form-cotiza':
-            $('.steps-process').children('.space').last().addClass('active')
-            $('.steps-process').children('.stp').last().children('.tag').addClass('active')
-            $('.steps-process').children('.stp').last().children('.number').addClass('active')
-            $('.m-crea-modal').fadeIn();
-            e.preventDefault()
-            timeooutredirect('cotizar')
-            break;
     }
 
 })
@@ -273,20 +123,6 @@ function itsempyornull(val) {
         return (true)
     } else {
         return (false)
-    }
-}
-function timeooutredirect(path) {
-    switch (path) {
-        case 'login':
-            setTimeout(() => {
-                window.location.href = "/?action=directlogin";
-            }, "5000")
-            break;
-        case 'cotizar':
-            setTimeout(() => {
-                window.location.href = "/historico-solicitudes.html";
-            }, "5000")
-            break;
     }
 }
 
@@ -309,12 +145,6 @@ const BtnRechazarMatricula = () => {
 const BtnCorregirMatricula = () => {
     $('#modal-corregir-matricula').fadeIn(500);
 }
-
-
-
-
-
-
 
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,58 +1,11 @@
-let countaddrs = 2;
-let countadelivery = 2;
+
 $(document).ready(function () {
     URL_OBEN = new URLSearchParams(window.location.search);
-    if (URL_OBEN.get('action') === "directlogin") {
-        $('.m-crea-section.right').addClass('inactive')
-        $('.m-crea-section.right').children('.m-crea__content').removeClass('active')
-        $('.m-crea-section.right').children('.m-crea__content:last-child').addClass('active')
-        $('.m-crea-section.left').addClass('inactive')
-    } else
-        if (URL_OBEN.get('action') === "openmodal") {
-            $('.m-crea-modal').fadeIn();
-        }
+
 })
 
 function OpenSection(e) {
     switch ($(e).attr('data-open')) {
-        case 'login':
-            new WOW().init();
-            if ($('.m-crea-section.left').hasClass('inactive')) {
-                $('.m-crea-section.left').removeClass('inactive')
-            } else {
-                $('.m-crea-section.left').addClass('inactive')
-            }
-            if ($('.m-crea-section.right').hasClass('inactive')) {
-                $('.m-crea-section.right').removeClass('inactive')
-            } else {
-                $('.m-crea-section.right').addClass('inactive')
-            }
-            if ($('.m-crea-section.right').children('.m-crea__content:last-child').hasClass('active')) {
-                $('.m-crea-section.right').children('.m-crea__content:first-child').addClass('active')
-                $('.m-crea-section.right').children('.m-crea__content:last-child').removeClass('active')
-            } else {
-                $('.m-crea-section.right').children('.m-crea__content:first-child').removeClass('active')
-                $('.m-crea-section.right').children('.m-crea__content:last-child').addClass('active')
-            }
-            break;
-        case 'register':
-            $('.m-crea-section.left').addClass('inactive')
-            window.location.href = "/register.html";
-            break;
-        //         case 'bussiness':
-        //             $('.m-crea-section.left').addClass('inactive')
-        //             window.location.href = "/register-bussiness.html" ;
-        //             break; 
-        //             case 'rescue':
-        //                 $('.m-crea-section.left').addClass('inactive')
-        //                 window.location.href = "/rescue-account.html";
-        //             break; 
-        //             case 'directlogin':
-        //                 $('.m-crea-section.left').addClass('inactive')
-        //                 window.location.href = "/?action=directlogin" ;
-        //     break;
-
-
         case 'profile':
             window.location.href = "/edit-profile.html";
             break;
@@ -60,13 +13,6 @@ function OpenSection(e) {
         case 'logout':
             window.location.href = "/index.html";
             break;
-        // case 'new-historical':
-        //     window.location.href = "/nueva-solicitud.html" ;
-        //     break;
-        // case 'historical':
-        //     window.location.href = "/historico-solicitudes.html" ;
-        //     break;
-
     }
 }
 
@@ -89,7 +35,6 @@ $(document).on('click', function (e) {
 $('.modal-btn__close').on('click', function () {
     $('.m-crea-modal').fadeOut();
 })
-
 
 /** Custom Select */
 $('.intranet-select').on('click', function () {
@@ -160,97 +105,6 @@ $('form').submit(function (e) {
                         return
                     }
             break;
-
-        /** recuperar contraseña validator */
-        case 'rescue':
-            var EMAIL = $('#email');
-            var MESSAGE = $('.text-warning')
-            if (!ValidateEmail(EMAIL.val())) {
-                MESSAGE.text('Correo inválido. Por favor ingrese un correo con el formato correcto.')
-                e.preventDefault()
-                EMAIL.addClass('border-warning')
-                return
-            } else {
-                $('.m-crea-modal').fadeIn();
-                e.preventDefault()
-                timeooutredirect('login')
-            }
-            break;
-        /** post recuperador de contraseña validator */
-        case 'post-rescue':
-            var CONTRASEÑA1 = $('#password');
-            var CONTRASEÑA2 = $('#password2');
-            var MESSAGE1 = $('#pasword1-message')
-            var MESSAGE2 = $('#pasword2-message')
-            if (itsempyornull(CONTRASEÑA1.val())) {
-                MESSAGE1.text('El campo no puede quedar vacío.')
-                e.preventDefault()
-                CONTRASEÑA1.addClass('border-warning')
-                return
-            } else if (CONTRASEÑA1.val().length < 8) {
-                MESSAGE1.text('Debe contenener un mínimo de 8 caracteres.')
-                e.preventDefault()
-                CONTRASEÑA1.addClass('border-warning')
-                return
-            } else
-                if (!(CONTRASEÑA1.val() === CONTRASEÑA2.val())) {
-                    MESSAGE2.text('Las contraseñas no coinciden.')
-                    CONTRASEÑA1.addClass('border-warning')
-                    CONTRASEÑA2.addClass('border-warning')
-                    e.preventDefault()
-                    return
-                } else {
-                    $('.m-crea-modal').fadeIn();
-                    e.preventDefault()
-                    timeooutredirect('login')
-                }
-            break;
-        /** Registrar validator */
-        case 'register':
-            var NAME = $('#name');
-            var LASTNAME = $('#lastname')
-            var EMAIL = $('#email')
-            var IDFISCAL = $('#fiscal')
-            var CARGO = $('#position');
-            var PHONE = $('#phone')
-            if (itsempyornull(NAME.val())) {
-                NAME.siblings('.text-warning').text('El campo nombre no puede quedar vacio')
-                NAME.addClass('border-warning')
-                e.preventDefault()
-                return
-            } else
-                if (itsempyornull(LASTNAME.val())) {
-                    LASTNAME.siblings('.text-warning').text('El campo apellido no puede quedar vacio')
-                    LASTNAME.addClass('border-warning')
-                    e.preventDefault()
-                    return
-                } else
-                    if (!ValidateEmail(EMAIL.val())) {
-                        EMAIL.siblings('.text-warning').text('Debe introducir una direccion de email válida')
-                        e.preventDefault()
-                        EMAIL.addClass('border-warning')
-                        return
-                    } /*else 
-            if (itsempyornull(IDFISCAL.val())) {
-                IDFISCAL.siblings('.text-warning').text('El campo id fiscal no puede quedar vacio') 
-                IDFISCAL.addClass('border-warning')
-                e.preventDefault()
-                return
-            } else
-            if (itsempyornull(CARGO.val())) {
-                CARGO.siblings('.text-warning').text('El campo cargo no puede quedar vacio') 
-                CARGO.addClass('border-warning')
-                e.preventDefault()
-                return
-            } else*/
-            /* if (itsempyornull(PHONE.val())) {
-                 PHONE.siblings('.text-warning').text('El campo telefono no puede quedar vacio') 
-                 PHONE.addClass('border-warning')
-                 e.preventDefault()
-                 return
-             }*/
-            break;
-        case 'form-cotiza':
             $('.steps-process').children('.space').last().addClass('active')
             $('.steps-process').children('.stp').last().children('.tag').addClass('active')
             $('.steps-process').children('.stp').last().children('.number').addClass('active')
@@ -275,20 +129,7 @@ function itsempyornull(val) {
         return (false)
     }
 }
-function timeooutredirect(path) {
-    switch (path) {
-        case 'login':
-            setTimeout(() => {
-                window.location.href = "/?action=directlogin";
-            }, "5000")
-            break;
-        case 'cotizar':
-            setTimeout(() => {
-                window.location.href = "/historico-solicitudes.html";
-            }, "5000")
-            break;
-    }
-}
+
 
 const ModalgenerarMatricula = () => {
     $('#modal-generarmatricula').fadeIn(500);
